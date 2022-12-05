6a60564710aa347a2f00b9afcfd65d6edb8886b0
diego.rigo <rigodiego@gmail.com>
2022-12-05 14:12:24 +0100

    Giorno 5 (prima parte)

---
+++ 2022/5/solution (part1).py
@@ -0,0 +1,44 @@
+import os
+import re
+
+location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+file1 = open(location + '\input.txt', 'r')
+Lines = file1.readlines()
+
+topInfo = True
+config = []
+N = 9
+for j in range(1,N+1):
+    config.append([])
+print(config)
+
+for line in Lines:
+    # print(line)
+    if (" 1 " in line):
+        topInfo = False
+
+    
+    if (topInfo):
+        for i in range(0,N):
+            print(str(i))
+            el = line[1+4*i]
+            if el.strip() != "":
+                config[i].insert(0,el)
+                print(f"{i} -> {el} -> {config[i]}")
+    else:
+        move = line.strip()
+        search = re.search('move (.*) from (.*) to (.*)', line)
+        if search != None:    
+            qty = int(search.group(1))
+            fromIndex = int(search.group(2))
+            toIndex = int(search.group(3))
+            print(f"Moving {qty} boxes from {fromIndex} to {toIndex}")
+            for box in range(0,qty):
+                removed = config[fromIndex-1].pop()
+                config[toIndex-1].append(removed)
+
+solution = ""
+for i in range(0,N):
+    solution += config[i].pop()
+
+print(solution)
--- 2022/download.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-day = 5
-url = "https://adventofcode.com/2022/day/" + str(day) + "/input"
-print(url)
-x = requests.get(url)
